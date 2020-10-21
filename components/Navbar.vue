8f75441dca4c3ff5c38fb6cb676c5c5842b24518
Stuart Payne <paynestu@gmail.com>
2020-10-21 14:14:23 +0100

    inital commit

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="nav">
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/portfolio">Portfolio</nuxt-link>
+    <a href="">Github</a>
+  </div>
+</template>
+
+<style scoped>
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+</style>
